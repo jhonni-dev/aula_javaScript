8980584f0e87beaaf4449d8f72bf41071e06e7fb
jhonni-dev <jhonni.dev@gmail.com>
2021-12-06 07:38:24 -0300

    Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+var frutas = [{nome: 'Maçã', cor:'Vermelha'}, {nome: 'Uva', cor:'Roxa'}]
+console.log(frutas)
+alert(frutas[1].nome)
+
+
+/*
+var fruta = {nome: 'Maçã', cor:'Vermelha'}
+console.log(fruta.nome)
+alert(fruta.cor)
+*/
+
+/*
+var lista = ["Maçã","Pêra", "Laranja"]
+lista.push('Uva') // Adiciona iten ao array
+//lista.pop() // Retira o último item do array
+console.log(lista)
+console.log(lista.length) // Exibe a qtd de elementos do array
+console.log(lista.reverse()) // Inverte a ordem dos elementos
+console.log(lista.toString()) // Converte o array em string
+console.log(lista.join(' - '))
+//alert(lista[0])
+*/
+
+/*
 var nome = "Jhonatas Santos"
 var n1 = 5
 var n2 = 3
@@ -10,3 +34,4 @@ console.log(n1 * n2)
 console.log(frase.replace("Japão", "Itália"))
 console.log(frase.toUpperCase())
 alert(frase.replace("Japão", "ITÁLIA"))
+*/
