5f8b89ea6172bf8e315830b9c0ffc3b602675a6a
jhonni-dev <jhonni.dev@gmail.com>
2021-12-06 08:27:40 -0300

    Condicionais, Laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,6 +1,41 @@
+var d = new Date ()
+alert (d.getMonth()+1) // getMonth sempre começa contar do 0, então sempre incremente +1
+alert (d.getDay())
+alert (d.getHours())
+alert (d.getMinutes())
+
+/*
+var count
+for (count = 0; count <=5; count ++) {
+    alert (count)
+}
+*/
+
+
+/*
+var count = 0
+while (count <= 5) {
+    console.log(count)
+    count++
+}
+*/
+
+
+/*
+var idade = prompt('Digite a sua idade: ') // Abre uma caixa de diálogo para entrada de dados
+if (idade >= 18) {
+    alert('Acesso autorizado')
+} else {
+    alert('Acesso negado, é preciso ter 18 anos de idade ou mais.')
+}
+*/
+
+
+/*
 var frutas = [{nome: 'Maçã', cor:'Vermelha'}, {nome: 'Uva', cor:'Roxa'}]
 console.log(frutas)
 alert(frutas[1].nome)
+*/
 
 
 /*
@@ -8,6 +43,7 @@ var fruta = {nome: 'Maçã', cor:'Vermelha'}
 console.log(fruta.nome)
 alert(fruta.cor)
 */
+
 
 /*
 var lista = ["Maçã","Pêra", "Laranja"]
