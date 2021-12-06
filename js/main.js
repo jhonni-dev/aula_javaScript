d3189e387976c05785a7f476feeaf172d835d071
jhonni-dev <jhonni.dev@gmail.com>
2021-12-06 09:19:00 -0300

    Funções e Variáveis globais e locais

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+function soma (n1, n2){
+    return n1 + n2
+}
+
+var validar = 0
+
+function validaIdade (idade){
+    if (idade >= 18) {
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar
+}
+var idade = prompt ('Por favor, digite a sua idade')
+validaIdade(idade)
+console.log(validar)
+
+//alert (soma(5,10))
+
+
+/*
 var d = new Date ()
 alert (d.getMonth()+1) // getMonth sempre começa contar do 0, então sempre incremente +1
 alert (d.getDay())
 alert (d.getHours())
 alert (d.getMinutes())
+*/
+
 
 /*
 var count
